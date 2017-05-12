d2c9be9701ac89bf73c2a802b5f788786311c281
dawei-yahoo <daweih@yahoo-inc.com>
2017-05-12 14:54:39 -0700

    Update SelectCommand.java

--- SelectCommand.java
+++ SelectCommand.java
@@ -1,5 +1,12 @@
-public class TableName {
-    private String;
-  
-    public 
+public class SelectCommand implements CommandGenerator {
+    private String tableName;
+    private String operatorName;
+    
+    SelectCommand(String table, String operator) {
+            
+    }
+    
+    public String commandGene () {
+        
+    }
 }
